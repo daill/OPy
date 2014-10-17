b3d5866e763b2914973034c5bcef628172eb941c
Christian Kramer <ck.daill@gmail.com>
2014-10-17 06:55:02 +0200

    added client and test package

---
+++ database/client/o_db_object.py
@@ -0,0 +1,8 @@
+__author__ = 'daill'
+
+class OObject(object):
+    """
+    This object has to be implemented by all object which should be auto saved and unfolded
+    """
+    def __init__(self):
+        pass
---
+++ database/test/o_connect_test.py
@@ -0,0 +1,11 @@
+import unittest
+
+__author__ = 'daill'
+
+class OConnectTest(unittest.TestCase):
+    def __init__(self):
+        print("hallo")
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
