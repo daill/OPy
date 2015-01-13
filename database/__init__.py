e712073de543f35170fc766afca40a9fb1391e12
Christian Kramer <ck.daill@gmail.com>
2015-01-13 21:05:55 +0100

    adds class information to logging output

--- database/__init__.py
+++ database/__init__.py
@@ -5,7 +5,7 @@ __author__ = 'daill'
 
 # set logging properties
 handler = logging.StreamHandler(sys.stdout)
-frm = logging.Formatter("%(asctime)s %(levelname)s: %(message)s", "%d.%m.%Y %H:%M:%S")
+frm = logging.Formatter("%(asctime)s %(filename)s line: %(lineno)d %(levelname)s: %(message)s", "%d.%m.%Y %H:%M:%S")
 handler.setFormatter(frm)
 logger = logging.getLogger()
 logger.addHandler(handler)
